e4abaaaaebd96ed9b9c1b331116922d5d1ce7148
Asis-hub <asis.reyes@hotmail.com>
2022-09-06 15:29:00 -0500

    Header reutilizable

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,12 +1,4 @@
-<div class="header">
-  <img class="logo" src="/images/d79a16b61de6b8760e0f8ed6c43ec2c0.png" alt="CEOA">
-  </h1>
-<a href="/">
-  <span class="left">
-  </span>
-</a>
-<h1 class="txtheader">SIMULE
-</h1>
-</div>
+<head>
+  <link rel="stylesheet" type="text/css" href="{{ url('css/header.css') }}">
+</head>
 
-<link rel="stylesheet" type="text/css" href="header.css"/>
