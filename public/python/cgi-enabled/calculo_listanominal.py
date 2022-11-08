a7644951bbfb3c92c74df4c666bc16b2497824f2
Asis-hub <asis.reyes@hotmail.com>
2022-11-07 22:43:40 -0600

    Calculo de listas nominales de acuerdo a genero

--- public/python/cgi-enabled/calculo_listanominal.py
+++ public/python/cgi-enabled/calculo_listanominal.py
@@ -5,8 +5,17 @@ import numpy as np
 #INE
 #https://ine.mx/transparencia/datos-abiertos/#/archivo/datos-por-rangos-de-edad-entidad-de-origen-y-sexo-del-padron-electoral-y-lista-nominal-2022
 
-#df = pd.read_excel(r'/home/asis/porSexoListaNominal.xlsx', sheet_name='pdln_edms_sexo_11032022')
-df = pd.read_excel(r'/home/asis/porSexoListaNominalFinalOctubre.xlsx', sheet_name='pdln_edms_sexo_20221028')
+df = pd.read_excel(r'/home/asis/porSexoListaNominal.xlsx', sheet_name='pdln_edms_sexo_11032022')
+#df = pd.read_excel(r'/home/asis/porSexoListaNominalFinalOctubre.xlsx', sheet_name='pdln_edms_sexo_20221028')
 
+suma_listahombres = df[df["NOMBRE\nMUNICIPIO"] == "XALAPA"]["LISTA\nHOMBRES"].sum()
+suma_listamujeres = df[df["NOMBRE\nMUNICIPIO"] == "XALAPA"]["LISTA\nMUJERES"].sum()
 suma_listanominal = df[df["NOMBRE\nMUNICIPIO"] == "XALAPA"]["LISTA\nNOMINAL"].sum()
-print(suma_listanominal)
+
+#print("Lista nominal hombres: " + suma_listahombres)
+#print("Lista nominal mujeres: " + suma_listamujeres)
+#print("Lista nominal total: " + suma_listanominal)
+
+print("Lista nominal hombres: " + str(suma_listahombres))
+print("Lista nominal mujeres: " + str(suma_listamujeres))
+print("Lista nominal total: " + str(suma_listanominal))
