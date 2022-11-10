c4db03ef1ddcdbf9610e4db1af0d1a6cea19a4d6
Asis-hub <asis.reyes@hotmail.com>
2022-11-10 11:46:57 -0600

    Cálculo de list por rango de edad y proporción

--- public/python/cgi-enabled/calculo_listanominal_rangoedad.py
+++ public/python/cgi-enabled/calculo_listanominal_rangoedad.py
@@ -5,7 +5,7 @@ import numpy as np
 #INE
 #https://ine.mx/transparencia/datos-abiertos/#/archivo/datos-por-rangos-de-edad-entidad-de-origen-y-sexo-del-padron-electoral-y-lista-nominal-2022
 
-df = pd.read_excel(r'/home/asis/porRangoEdadEjemplo.xlsx', sheet_name='pdln_edms_sexo_11032022')
+df = pd.read_excel(r'/home/asis/EjemploPorRangoEdad.xlsx', sheet_name='pdln_edms_re_sexo_11032022')
 
 #suma_listahombres18 = df[df["NOMBRE\nMUNICIPIO"] == "XALAPA"]["PADRON_18_HOMBRES"].sum()
 #suma_listamujeres18 = df[df["NOMBRE\nMUNICIPIO"] == "XALAPA"]["PADRON_18_MUJERES"].sum()
@@ -61,6 +61,33 @@ suma_listamujeres65_mas = df[df["NOMBRE\nMUNICIPIO"] == "XALAPA"]["LISTA_65_Y_MA
 #print("Lista nominal mujeres: " + suma_listamujeres)
 #print("Lista nominal total: " + suma_listanominal)
 
-print("Lista nominal hombres: " + str(suma_listahombres18 + suma_listahombres19 + suma_listahombres20_24 + suma_listahombres25_29 + suma_listahombres30_34 + suma_listahombres30_34 + suma_listahombres35_39 + suma_listahombres40_44 + suma_listahombres45_49 + suma_listahombres50_54 + suma_listahombres55_59 + suma_listahombres60_64 + suma_listahombres65_mas))
-print("Lista nominal mujeres: " + str(suma_listamujeres18 + suma_listamujeres19 + suma_listamujeres20_24 + suma_listamujeres25_29 + suma_listamujeres30_34 + suma_listamujeres30_34 + suma_listamujeres35_39 + suma_listamujeres40_44 + suma_listamujeres45_49 + suma_listamujeres50_54 + suma_listamujeres55_59 + suma_listamujeres60_64 + suma_listamujeres65_mas))
+#print("Lista nominal hombres: " + str(suma_listahombres18 + suma_listahombres19 + suma_listahombres20_24 + suma_listahombres25_29 + suma_listahombres30_34 + suma_listahombres30_34 + suma_listahombres35_39 + suma_listahombres40_44 + suma_listahombres45_49 + suma_listahombres50_54 + suma_listahombres55_59 + suma_listahombres60_64 + suma_listahombres65_mas))
+#print("Lista nominal mujeres: " + str(suma_listamujeres18 + suma_listamujeres19 + suma_listamujeres20_24 + suma_listamujeres25_29 + suma_listamujeres30_34 + suma_listamujeres30_34 + suma_listamujeres35_39 + suma_listamujeres40_44 + suma_listamujeres45_49 + suma_listamujeres50_54 + suma_listamujeres55_59 + suma_listamujeres60_64 + suma_listamujeres65_mas))
 #print("Lista nominal todos: " + str(suma_listahombres18 + suma_listahombres19 + suma_listahombres20_24 + suma_listahombres25_29 + suma_listahombres30_34 + suma_listahombres30_34 + suma_listahombres35_39 + suma_listahombres40_44 + suma_listahombres45_49 + suma_listahombres50_54 + suma_listahombres55_59 + suma_listahombres60_64 + suma_listahombres65_mas)+(suma_listamujeres18 + suma_listamujeres19 + suma_listamujeres20_24 + suma_listamujeres25_29 + suma_listamujeres30_34 + suma_listamujeres30_34 + suma_listamujeres35_39 + suma_listamujeres40_44 + suma_listamujeres45_49 + suma_listamujeres50_54 + suma_listamujeres55_59 + suma_listamujeres60_64 + suma_listamujeres65_mas))
+
+lista_hombres_18_24 = int(suma_listahombres18 + suma_listahombres19 + suma_listahombres20_24)
+lista_mujeres_18_24 = int(suma_listamujeres18 + suma_listamujeres19 + suma_listamujeres20_24)
+print("Lista nominal de 18-24 años hombres: " + (str(lista_hombres_18_24)))
+print("Lista nominal de 18-24 años mujeres: " + (str(lista_mujeres_18_24)))
+
+
+print("Lista nominal de 25-34 años hombres: " + (str(int(suma_listahombres25_29 + suma_listahombres30_34))))
+print("Lista nominal de 25-34 años mujeres: " + (str(int(suma_listamujeres25_29 + suma_listamujeres30_34))))
+
+print("Lista nominal de 35-49 años hombres: " + (str(int(suma_listahombres35_39 + suma_listahombres40_44 + suma_listahombres45_49))))
+print("Lista nominal de 35-49 años mujeres: " + (str(int(suma_listamujeres35_39 + suma_listamujeres40_44 + suma_listamujeres45_49))))
+
+print("Lista nominal de 50-64 años hombres: " + (str(int(suma_listahombres50_54 + suma_listahombres55_59 + suma_listahombres60_64))))
+print("Lista nominal de 50-64 años mujeres: " + (str(int(suma_listamujeres50_54 + suma_listamujeres55_59 + suma_listamujeres60_64))))
+
+print("Lista nominal de 25-34 años hombres: " + (str(int(suma_listahombres65_mas))))
+print("Lista nominal de 25-34 años mujeres: " + (str(int(suma_listamujeres65_mas))))
+
+listaTotal = (int(suma_listahombres18 + suma_listahombres19 + suma_listahombres20_24 + suma_listamujeres18 + suma_listamujeres19 + suma_listamujeres20_24 + suma_listahombres25_29 + suma_listahombres30_34 + suma_listamujeres25_29 + suma_listamujeres30_34 + suma_listahombres35_39 + suma_listahombres40_44 + suma_listahombres45_49 + suma_listamujeres35_39 + suma_listamujeres40_44 + suma_listamujeres45_49 + suma_listahombres50_54 + suma_listahombres55_59 + suma_listahombres60_64 + suma_listamujeres50_54 + suma_listamujeres55_59 + suma_listamujeres60_64 + suma_listahombres65_mas + suma_listamujeres65_mas))
+
+print("La lista nominal total es: " + str(listaTotal))
+
+print("La proporción para hombres de 18 a 24 años sería: " + (str(round(float(lista_hombres_18_24/listaTotal),3))))
+print("La proporción para mujeres de 18 a 24 años sería: " + (str(round(float(lista_mujeres_18_24/listaTotal),3))))
+
+print("Si fueran 600 encuestas por realizar, a los hombres de 18 a 24 años les tocaría: " + (str(int((round(float(lista_hombres_18_24/listaTotal),3))*600))))
